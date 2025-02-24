1c70c19b0297334d042fa16e49257aafb7596e00
Holger Veltrup <veltrup@sitepark.com>
2025-02-24 07:29:41 +0100

    feat: get user with roles, get roles with privileges

---
+++ src/main/java/com/sitepark/ies/userrepository/core/usecase/GetPrivilegesByIds.java
@@ -0,0 +1,30 @@
+package com.sitepark.ies.userrepository.core.usecase;
+
+import com.sitepark.ies.userrepository.core.domain.entity.Privilege;
+import com.sitepark.ies.userrepository.core.domain.exception.AccessDeniedException;
+import com.sitepark.ies.userrepository.core.port.AccessControl;
+import com.sitepark.ies.userrepository.core.port.PrivilegeRepository;
+import jakarta.inject.Inject;
+import java.util.List;
+
+public final class GetPrivilegesByIds {
+
+  private final PrivilegeRepository repository;
+
+  private final AccessControl accessControl;
+
+  @Inject
+  protected GetPrivilegesByIds(PrivilegeRepository repository, AccessControl accessControl) {
+    this.repository = repository;
+    this.accessControl = accessControl;
+  }
+
+  public List<Privilege> getPrivilegesByIds(List<String> ids) {
+
+    if (!this.accessControl.isPrivilegeReadable()) {
+      throw new AccessDeniedException("Not allowed to read privileges");
+    }
+
+    return this.repository.getByIds(ids);
+  }
+}
