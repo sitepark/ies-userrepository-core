39043ea02ab44434a31dd4863940b5bbb97a1353
Holger Veltrup <veltrup@sitepark.com>
2025-08-29 07:48:32 +0200

    fix: syntax error, exception message

--- src/main/java/com/sitepark/ies/userrepository/core/domain/exception/AnchorNotFoundException.java
+++ src/main/java/com/sitepark/ies/userrepository/core/domain/exception/AnchorNotFoundException.java
@@ -3,9 +3,9 @@ package com.sitepark.ies.userrepository.core.domain.exception;
 import com.sitepark.ies.userrepository.core.domain.entity.Anchor;
 
 /**
- * The <code>AnchorNotFoundException</code> exception is thrown when an anchor cannot be found
- * or does not exist in the system, typically when trying to access or manipulate an
- * anchor that is not present.
+ * The <code>AnchorNotFoundException</code> exception is thrown when an anchor cannot be found or
+ * does not exist in the system, typically when trying to access or manipulate an anchor that is not
+ * present.
  */
 public class AnchorNotFoundException extends UserRepositoryException {
 
@@ -23,6 +23,6 @@ public class AnchorNotFoundException extends UserRepositoryException {
 
   @Override
   public String getMessage() {
-    return "User with anchor " + this.anchor + " not found";
+    return "Anchor " + this.anchor + " not found";
   }
 }
