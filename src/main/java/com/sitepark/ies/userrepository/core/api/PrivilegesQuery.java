1e0e12191265aa22e5b086e11ea9a3aff3e28875
Holger Veltrup <veltrup@sitepark.com>
2025-10-02 10:52:12 +0200

    feat: add PrivilegesQuery.getRolesAssignByPrivileges()

--- src/main/java/com/sitepark/ies/userrepository/core/api/PrivilegesQuery.java
+++ src/main/java/com/sitepark/ies/userrepository/core/api/PrivilegesQuery.java
@@ -2,11 +2,14 @@ package com.sitepark.ies.userrepository.core.api;
 
 import com.sitepark.ies.userrepository.core.domain.entity.Privilege;
 import java.util.List;
+import java.util.Map;
 
 public interface PrivilegesQuery {
   List<Privilege> getPrivilegesByUserId(String userId);
 
   List<String> getRolesAssignByPrivilege(String privilegeId);
+
+  Map<String, List<String>> getRolesAssignByPrivileges(List<String> privilegeIds);
 
   List<Privilege> getPrivilegesByDataField(String jsonPath, List<String> fieldValues);
 }
